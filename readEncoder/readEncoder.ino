22f2f724122a478131075ca3cec26f8b6e19cf64
nicolecam <niccam18@gmail.com>
2022-03-19 12:10:06 -0700

    Turned readEncoder into a function for PID program

--- readEncoder/readEncoder.ino
+++ readEncoder/readEncoder.ino
@@ -1,38 +1,47 @@
-int parallel = 1;  // pin #
-int clk = 2;       // pin #
-int dataout = 3;   // pin #
-
-
-
-byte switcharray = 0;
+// declare pins (these can be changed)
+int parallel = 4;    
+int clk = 5;
+int serialData = 6;
 
 void setup() {
+  // baud rate
+  Serial.begin(9600);
+  
+  // setup io pins
   pinMode(parallel, OUTPUT);
   pinMode(clk, OUTPUT);
-  pinMode(dataout, INPUT);
-
-  Serial.begin(9600);   // baud rate
+  pinMode(serialData, INPUT);
 
 }
 
 void loop() {
+  // call function
+  int encoder_num = readEncoder();
 
-  byte value = 0;
-  byte tempbit = 0;
+}
+
+
+// --- Read from encoder function: ---
+int readEncoder(){
+
+  int8_t value = 0;
+  int8_t tempbit = 0;
   digitalWrite(parallel, 0);  // enable parallel inputs
   digitalWrite(clk, 0);       // clk low
   digitalWrite(clk, 1);       // clk high, load data
-  digitalWrite(parallel, 1);  // disable parallel inputs
+  digitalWrite(parallel, 1);  // disable parallel inputs 
+
+  // read in 8 bits:
+  for(int i = 0; i < 8; i++){
+    tempbit = digitalRead(serialData);
+    value = value + (tempbit << i); // shift bit and add it
+    digitalWrite(clk,0);          // clk low
+    digitalWrite(clk,1);          // clk high to get next bit
+  }
+
+  // cast int8_t to int:
+  int encoder_value = (int8_t) value; // signed -> negative if backwards from home, positive if forwards from home
   
-
-    for(int j = 0; j<8; j++) {
-      tempbit = digitalRead(dataOut);
-      value = value + (tempbit << j);  // shift bit and add it
-      digitalWrite(clk, 0);     // clk low 
-      digitalWrite(clk, 1);     // clk high to get next bit
-    }
-
- 
+  return encoder_value;
   
-
-}l
+}//end readEncoder
