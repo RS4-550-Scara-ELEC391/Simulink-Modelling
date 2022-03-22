0e680ab9643fd5afadc7b2c98b231a0a1ba8f800
Davin Birdi <davin.birdi@hotmail.ca>
2022-03-21 17:21:52 -0700

    I'm just going to commit - This is WIP. I want to see what nicole's model looks like

--- draft_model_variables.m
+++ draft_model_variables.m
@@ -8,8 +8,8 @@ clc
 open_system('draft_arm_model.slx')
 
 % "Make good Choices" - dad:
-J1 = 1;
-J2 = 2;
+J1 = 3;
+J2 = 3;
 loaded = 1;
 
 
@@ -34,7 +34,21 @@ elseif (J1 == 2)
     ka = km;        % [Nm/A]
     motor_name = 'RE-35'
     gear_i = 75;
+
+elseif (J1 == 3)
+    V_nom = 12;
+    R = 0.115;
+    L = 0.0245e-3; % [H]
+    km = 16.4e-3;
+    ka = km;
+    motor_name = 'RE-40'
+    part_number = '148866';
+    gear_i = 100;
+    %https://www.max 
+    % ongroup.com/maxon/view/category/gear?etcc_cu=onsite&etcc_med_onsite=Product&etcc_cmp_onsite=Planetary+Gearheads+(GP)&etcc_plc=Overview-Page-Gears&etcc_var=%5bcom%5d%23en%23_d_&target=filter&filterCategory=planetary
 end
+
+
 
 
 % Joint 2 Motor:
@@ -59,6 +73,17 @@ elseif (J2 == 2)
     motor_name2 = 'RE-35'
     gear_i2 = 1;
 
+elseif (J2 == 3)
+    % Maxon motor: RE-30
+    % variables from data sheet:
+    V_nom2 = 12;         % [V]
+    R2 = 0.2;           % [Ohm]
+    L2 = 0.0345e-3;     % [H]
+    km2 = 30.9*1e-3;%13.9*1e-3;     % [Nm/A]
+    ka2 = km;
+    motor_name2 = 'RE-30'
+    part_number2 = '268193';
+    gear_i2 = 25;
 end
 
 % Mechanical Variables from Datasheet
@@ -69,21 +94,39 @@ Jload = 23.44*0.0001; % calculated from solidworks model
 J = Jrot + Jload;   % [gm^2]
 J = J/1000;         % [Kgm^2]
 
+K1 = 39.8;
 
 % P-Controller:
-Kp = 10;
-Ki = 0;
-Kd = 0;
+Kp = K1 * 0.2548;
+Ki = K1 * 1;
+Kd = K1 * 0.0162;
 
 % P-Controller:
-Kp2 = 1;
-Ki2 = 0;
-Kd2 = 1;
+K2 = 1.25;
+
+Kp2 = K2 * 0.1921;
+Ki2 = K2 * 1;
+Kd2 = K2 * 0.0091;
 
 
+time = 0:0.001:1.999;
+path = trajectories(pi, 2, 0.001);
+path2 = trajectories(pi, 2, 0.001);
+
+
+% H-Bridge Amplifier 2nd Order Approximation:
+amplifier_num = [2.149e7];
+amplifier_denom = [1 655.7 5.52e6];
+
+
+foldername='nicoleTuning/';
 % Parameter Plot: Name the Output Files:
-mkdir(strcat('out/',motor_name))
-name = sprintf(strcat('out/', motor_name,'/Kp=%03d-Ki=%03d-Kd=%03d-Gear=%03d_data'), Kp, Ki, Kd, gear_i);
+%mkdir(strcat('out/',motor_name))
+mkdir(strcat('out/', foldername))
+%name = sprintf(strcat('out/', motor_name,'/Kp=%03d-Ki=%03d-Kd=%03d-Gear=%03d_data'), Kp, Ki, Kd, gear_i);
+name = sprintf(strcat('out/', foldername, motor_name, '-', motor_name2, '-', 'Gear1=%03d-Gear2=%03d-', 'loaded-%d', '_data'), gear_i,gear_i2, loaded);
+
+
 % Set Simulink Model simOut Blocks to write to specific file name:
-set_param('draft_arm_model/dataOut','filename', name)
+set_param('draft_arm_model/dataOut','filename', name);
 set_param('draft_arm_model/dataOut','MatrixName', 'data')
--- out/ParameterPlot.m
+++ out/ParameterPlot.m
@@ -9,28 +9,35 @@ gearElec = [ ...
     load("out\Kp2Ki0Kd1-Gear25\MotorElec.mat") ...
     load("out\Kp2Ki0Kd1-Gear50\MotorElec.mat") ...
     load("out\Kp2Ki0Kd1-Gear75\MotorElec.mat") ...
+    load("out\Kp20Ki0Kd1-Gear75\MotorElec.mat") ...
+    load("out\Kp20Ki0Kd1-Gear25\MotorElec.mat") ...
 ];
 gearMech = [ ...
     load('out\Kp2Ki0Kd1-Gear10\MotorMech.mat') ...
     load('out\Kp2Ki0Kd1-Gear25\MotorMech.mat') ...
     load('out\Kp2Ki0Kd1-Gear50\MotorMech.mat') ...
     load('out\Kp2Ki0Kd1-Gear75\MotorMech.mat') ...
+    load('out\Kp20Ki0Kd1-Gear75\MotorMech.mat') ...
+    load('out\Kp20Ki0Kd1-Gear25\MotorMech.mat') ...
 ];
 output = [ ...
     load("out\Kp2Ki0Kd1-Gear10\output.mat") ...
     load("out\Kp2Ki0Kd1-Gear25\output.mat") ...
     load("out\Kp2Ki0Kd1-Gear50\output.mat") ...
     load("out\Kp2Ki0Kd1-Gear75\output.mat") ...
+    load("out\Kp20Ki0Kd1-Gear75\output.mat") ...
+    load("out\Kp20Ki0Kd1-Gear25\output.mat") ...
 ];
+
+start = 4;
 
 figure(1)
 clf
-plot(gearElec(1).data.I)
 hold on
-plot(gearElec(2).data.I)
-plot(gearElec(3).data.I)
-plot(gearElec(4).data.I)
-lgd = legend('10', '25', '50', '75');
+for ii = start:size(output,2)
+    plot(gearElec(ii).data.I)
+end
+lgd = legend('10', '25', '50', '75', '75-Kp20');
 title(lgd, 'Gear Ratio');
 title('Current v. Time with Varying Gear Ratios')
 xlim([0.4 0.75])
@@ -38,11 +45,10 @@ xlim([0.4 0.75])
 
 figure(2)
 clf
-plot(output(1).data.J1_angle)
 hold on
-plot(output(2).data.J1_angle)
-plot(output(3).data.J1_angle)
-plot(output(4).data.J1_angle)
+for ii = start:size(output,2)
+    plot(output(ii).data.J1_angle)
+end
 lgd = legend('10', '25', '50', '75');
 title(lgd, 'Gear Ratio');
 title('J1 Angle v. Time with Varying Gear Ratios')
@@ -51,8 +57,8 @@ title('J1 Angle v. Time with Varying Gear Ratios')
 figure(3)
 clf
 hold on
-for i = 1:1:4
-plot(gearMech(i).data.signal2.Motor_Speed)
+for ii = start:size(output,2)
+plot(gearMech(ii).data.signal2.Motor_Speed)
 end
 xlim([0.4 0.75])
 gd = legend('10', '25', '50', '75');
